a8afef42f8eee33246cb3c067e684dca2f4593d5
Nathan Panzer <wasabimath@gmail.com>
2021-09-24 13:28:18 -0600

    Export statistics
    Took 1 hour 7 minutes

--- src/main/java/com/thekingelessar/assault/commands/CommandAssaultAdmin.java
+++ src/main/java/com/thekingelessar/assault/commands/CommandAssaultAdmin.java
@@ -1,5 +1,7 @@
 package com.thekingelessar.assault.commands;
 
+import com.thekingelessar.assault.Assault;
+import com.thekingelessar.assault.database.AssaultTableManager;
 import com.thekingelessar.assault.game.GameInstance;
 import com.thekingelessar.assault.game.GameStage;
 import com.thekingelessar.assault.game.player.GamePlayer;
@@ -10,12 +12,61 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.io.File;
+import java.util.Locale;
+import java.util.logging.Level;
+
 public class CommandAssaultAdmin implements CommandExecutor
 {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
     {
         if (args == null || args.length == 0) return false;
+        
+        if (args[0].equalsIgnoreCase("export"))
+        {
+            File exportFile;
+            if (args.length >= 2)
+            {
+                StringBuilder filename = new StringBuilder();
+                
+                for (String s : args)
+                {
+                    String arg = s + " ";
+                    filename.append(arg);
+                }
+                
+                exportFile = AssaultTableManager.getInstance().exportStatistics(new File("plugins/Assault/exported/" + filename.toString()));
+            }
+            else
+            {
+                exportFile = AssaultTableManager.getInstance().exportStatistics(null);
+            }
+            
+            if (sender instanceof Player)
+            {
+                Player player = (Player) sender;
+                if (exportFile == null)
+                {
+                    player.sendMessage(Assault.ASSAULT_PREFIX + "Error exporting statistics—see server log for more details.");
+                }
+                else
+                {
+                    player.sendMessage(Assault.ASSAULT_PREFIX + "Statistics exported to " + exportFile + "!");
+                }
+            }
+            
+            if (exportFile == null)
+            {
+                Assault.INSTANCE.getLogger().log(Level.INFO, "Could not export statistics to " + exportFile + ".");
+            }
+            else
+            {
+                Assault.INSTANCE.getLogger().log(Level.INFO, "Statistics exported to " + exportFile + ".");
+            }
+            
+            return true;
+        }
         
         if (sender instanceof Player)
         {
@@ -25,6 +76,7 @@ public class CommandAssaultAdmin implements CommandExecutor
             }
             
             Player player = (Player) sender;
+            
             GameInstance testInstance = GameInstance.getPlayerGameInstance(player);
             if (testInstance != null)
             {
